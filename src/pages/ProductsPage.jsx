63c960e00dc9f7aac63aba6194cf8829017d448a
Mariam-1-m <mariumaabdelrahman7@gmail.com>
2026-07-23 16:38:16 +0300

    render product component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,21 @@
-function ProductsPage(){
 
+// import Searchbar from './../components/Shop/Searchbar';
+import ProductsCards from './../components/Shop/ProductsCards';
+// import Sidebar from './../components/Shop/Sidebar';
+
+function ProductsPage(){
+    return (
+        <div className="">
+            {/* <div>
+            <Searchbar/>
+               
+            </div> */}
+         <div className="flex">
+             {/* <Sidebar/> */}
+            <ProductsCards/>
+         </div>
+            
+        </div>
+    );
 }
 export default ProductsPage;
