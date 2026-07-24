832faa9647bfd875084ad5ba5adc5255b969f837
HabibaHesham19 <habibaheshammohamedewais@gmail.com>
2026-07-24 20:13:17 +0300

    update Home page

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { Cable } from 'lucide-react';
 import { HouseWifi } from 'lucide-react';
 import { Dumbbell } from 'lucide-react';
+
 export default function HowItWorks() {
   return (
-    <div className="ShopSection bg-[#1E293B]  flex flex-col items-center justify-center h-[75vh] p-6">
+    <div className="ShopSection bg-[#161F32]  flex flex-col items-center justify-center h-[75vh] p-6">
 
   {/* Header */}
   <div className="container1 text-center mb-12">
-    <h2 className="ShopHeading2 text-4xl md:text-5xl font-extrabold text-slate-900 tracking-tight">
+    
+    <h2 className="ShopHeading2 text-4xl  ">
       How It Works
     </h2>
     <p className="text-slate-500 mt-2 text-lg">Simple steps to get your items delivered</p>
   </div>
 
   {/* Container2 Categories */}
-  <div className="container2 grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-7xl px-8">
+  <div className="container2 grid grid-cols-1 md:grid-cols-3  w-full max-w-7xl  gap-50 ">
 
     {/* Group 1 */}
     <div className="ShopCard1 flex flex-col items-center text-center gap-3 p-4">
@@ -23,7 +25,7 @@ export default function HowItWorks() {
         <Cable className="w-12 h-12 text-blue-950" strokeWidth={2} />
       </div>
       <p className="text-2xl font-bold capitalize">Browse Products</p>
-      <p className="text-base text-slate-600 leading-relaxed max-w-xs">
+      <p className="text-base text-slate-600 ">
         Explore our wide range of premium products
       </p>
     </div>
--- src/components/Home/Subscribtion.jsx
+++ src/components/Home/Subscribtion.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
-
+import { Mail } from 'lucide-react';
 export default function Subscribtion() {
   return (
-      <div className="Section">
-        <div className="Container">
-          <div className="container">
-            <h3>Stay Updated</h3>
-            <p>Subscribe to our newsletter and get exclusive deals and new arrivals first.</p>
-            <input type="text" placeholder='Enter your email'   name="" id=""  />
-            <button>Subscribe</button>
-          </div>
-        </div>
-      </div>
+    <div className="SubscribtionSection bg-[#0F172A] h-[80vh] flex flex-col items-center justify-center">
+      {/* SubscribtionContainer1 */}
+      <div className="SubscribtionContainer1 bg-[#463CD1]  w-[90%] h-[75%] p-25 flex flex-col items-center justify-center   rounded-3xl text-white">
+        <div className="SubscribtionContainer2 flex flex-col items-center text-center ">{/* SubscribtionContainer2 */}
+          <Mail className="size-11  -mt-5 " />
+          <h3 className=" text-3xl font-bold  mt-5">Stay Updated</h3>
+          <p className='text-[#e0e7ff] mt-5 '>Subscribe to our newsletter and get exclusive deals and <br /> new arrivals first.</p>
+
+          <div className="SubscribtionContainer3 flex gap-3 mt-10   w-full  h-13  max-w-md">{/* SubscribtionContainer3 */}
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className=" bg-[#ffffff1a]  p-2  w-90  rounded-lg  focus:outline-none focus:ring-2 focus:ring-white/50"
+            />
+            <button className=" font-semibold bg-white w-40 text-[#463CD1] px-4 py-2 rounded-lg hover:bg-gray-100 transition-colors">
+              Subscribe
+            </button>
+          </div>{/*SubscribtionContainer3  */}
+        </div>{/* SubscribtionContainer2 */}
+      </div>{/* End SubscribtionContainer1 */}
+      <hr className='bg-white' />
+      {/* End SubscribtionSection */}
+    </div>
   )
 }
