f975ef029a54f1d822a80c122611e5e4cb85c032
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-26 00:12:16 +0300

    Test

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
-import { Navigate, useLocation } from "react-router-dom"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
 
-export default function ProtectedRoute({children}) {
+export default function ProtectedRoute() {
     const location = useLocation()
     const token = localStorage.getItem("token")
-    return token ? children : <Navigate to="/login" replace state={{from: location}}/>
+    return token ? <Outlet /> : <Navigate to="/login" replace state={{from: location}}/>
 }
