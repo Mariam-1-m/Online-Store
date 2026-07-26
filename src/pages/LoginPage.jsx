9fcbb1cc768dabb29ad66e49752b95d43bb284a9
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-26 20:41:29 +0300

    Test

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,10 @@ export default function LoginPage() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
-    const token = localStorage.getItem("token");
   const from = location.state?.from?.pathname || "/";
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
     if (token) navigate("/", {replace: true})
   },[navigate,token])
 
